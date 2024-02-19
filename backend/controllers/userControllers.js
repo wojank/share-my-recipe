d1001c2c34aa26e2662edc8c9e992838b9a3d364
Miglanc <poncjuszmiglanc@gmail.com>
2024-02-19 14:21:50 +0100

    4th; userControllers: added loginUser and logoutUser controllers

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -22,14 +22,12 @@ const registerUser = async (req, res) => {
 
     if (newUser) {
       createToken(res, newUser._id);
-      res
-        .status(200)
-        .json({
-          message: "user registered succesfully",
-          userId: newUser._id,
-          userLogin: newUser.login,
-          userEmail: newUser.email,
-        });
+      res.status(200).json({
+        message: "user registered succesfully",
+        userId: newUser._id,
+        userLogin: newUser.login,
+        userEmail: newUser.email,
+      });
     } else {
       res.status(400);
       throw new Error("ni ma i cześc");
@@ -41,8 +39,32 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.validatePassword(password))) {
+    createToken(res, user._id);
+    res
+      .status(200)
+      .json({ userId: user._id, userLogin: user.login, userEmail: user.email });
+  } else {
+    res.status(401).json({ message: "invalid email or password" });
+    throw new Error("invalid email or password");
+  }
+};
+
+const logoutUser = (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expiresIn: new Date(0),
+  });
+  res.status(200).json({ message: "wylogowano poprawnie" });
+};
+
 const getUser = (req, res) => {
   res.status(200).json({ message: "nasz user" });
 };
 
-module.exports = { registerUser, getUser };
+module.exports = { registerUser, loginUser, logoutUser, getUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, getUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+} = require("../controllers/userControllers");
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 router.get("/", getUser);
 
 module.exports = router;
