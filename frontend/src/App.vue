7d1dde398642080127121540b96e7d0f802f1152
Miglanc <poncjuszmiglanc@gmail.com>
2024-03-14 23:22:04 +0100

    7th, initialized frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="navbar">
+    <div class="navbar__wrapper">
+      <div class="nav-wrapper">
+        <div class="brand">
+          <h2 class="brand__logo">MyRecipe</h2>
+        </div>
+        <nav class="nav">
+          <ul class="nav__list">
+            <li class="nav__list-item">Newsy</li>
+            <li class="nav__list-item">Desery</li>
+            <li class="nav__list-item">Koktajle</li>
+            <li class="nav__list-item">Przepisy</li>
+          </ul>
+        </nav>
+        <div class="hamburger">
+          <span class="hamburger__bar"></span>
+        </div>
+      </div>
+      <button class="cta-sign">Sign Up</button>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+@import "../assets/sass/components/navbar.scss";
+</style>
