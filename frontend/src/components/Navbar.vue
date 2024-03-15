2982a4178929d7dadfedfb200fafe58ca640a5b6
Miglanc <poncjuszmiglanc@gmail.com>
2024-03-15 20:35:02 +0100

    8th, created Navbar component

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isOpen = ref(false);
+</script>
 
 <template>
   <header class="navbar">
@@ -7,16 +11,24 @@
         <div class="brand">
           <h2 class="brand__logo">MyRecipe</h2>
         </div>
-        <nav class="nav">
+        <nav :class="[{ open: isOpen }, 'nav']">
           <ul class="nav__list">
-            <li class="nav__list-item">Newsy</li>
-            <li class="nav__list-item">Desery</li>
-            <li class="nav__list-item">Koktajle</li>
-            <li class="nav__list-item">Przepisy</li>
+            <li class="nav__list-item">
+              <a href="#" class="nav__list-link">Newsy</a>
+            </li>
+            <li class="nav__list-item">
+              <a href="#" class="nav__list-link">Desery</a>
+            </li>
+            <li class="nav__list-item">
+              <a href="#" class="nav__list-link">Koktajle</a>
+            </li>
+            <li class="nav__list-item">
+              <a href="#" class="nav__list-link">Przepisy</a>
+            </li>
           </ul>
         </nav>
-        <div class="hamburger">
-          <span class="hamburger__bar"></span>
+        <div class="hamburger" @click="isOpen = !isOpen">
+          <span :class="[{ rotate: isOpen }, 'hamburger__bar']"></span>
         </div>
       </div>
       <button class="cta-sign">Sign Up</button>
