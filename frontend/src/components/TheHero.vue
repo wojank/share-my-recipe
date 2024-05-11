618d0994406c5da164a9ad0a43a2e2d1c440bda5
Miglanc <poncjuszmiglanc@gmail.com>
2024-05-11 22:58:55 +0200

    18th, frontend: created TheHero component and the-hero styles

--- frontend/src/components/TheHero.vue
+++ frontend/src/components/TheHero.vue
@@ -1,6 +1,28 @@
 <script setup lang="ts"></script>
 <template>
-  <section class="hero"></section>
+  <section class="hero">
+    <div class="hero__container">
+      <div class="main">
+        <div>
+          <h1 class="main__headline">
+            znajdź przepis<br />
+            który cię <br />zainspiruje
+          </h1>
+          <p class="main__paragraph">
+            Lub załóż konto i zainspiruj innych <br />
+            publikując własne przepisy
+          </p>
+        </div>
+        <div class="cta">
+          <button class="cta__btn cta__btn--primary">zarejestruj się</button>
+          <button class="cta__btn cta__btn--secondary">
+            przeglądaj przepisy
+          </button>
+        </div>
+      </div>
+      <img class="hero__image" src="/hero.jpg" alt="przepisy.pl" />
+    </div>
+  </section>
 </template>
 <style scoped>
 @import "../assets/sass/components/the-hero.scss";
