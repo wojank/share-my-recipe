7ef905c8134fe17b2e48f85f03dd0baae5e93272
Miglanc <poncjuszmiglanc@gmail.com>
2024-08-02 00:10:52 +0200

    24rd, frontend: created CategoryCarousel component

--- frontend/src/components/CategoryCard.vue
+++ frontend/src/components/CategoryCard.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Config {
+  name: String;
+  link?: String;
+}
+const props = defineProps<Config>();
+const { name, link } = props;
+</script>
 <template>
   <div class="cat-card">
     <img class="cat-card__img" src="/categoryfake.jpeg" alt="przepisy.pl" />
-    <h4 class="cat-card__title">kuchnia indyjska</h4>
+    <h4 class="cat-card__title">{{ name }}</h4>
     <span class="cat-card__separator"></span>
   </div>
 </template>
--- frontend/src/components/CategoryCarousel.vue
+++ frontend/src/components/CategoryCarousel.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
 import CategoryCard from "./CategoryCard.vue";
+const categoryConfig = [
+  {
+    name: "Kuchnia Indyjska",
+    link: "kuchnia-indyjska",
+  },
+  {
+    name: "Kuchnia Włoska",
+    link: "kuchnia-indyjska",
+  },
+  {
+    name: "Kuchnia Azjatycka",
+    link: "kuchnia-indyjska",
+  },
+  {
+    name: "Kuchnia Francuska",
+    link: "kuchnia-indyjska",
+  },
+  {
+    name: "Kuchnia Meksykańska",
+    link: "kuchnia-indyjska",
+  },
+];
 </script>
 <template>
   <section class="cat-carousel">
-    <CategoryCard />
+    <CategoryCard v-for="item in categoryConfig" v-bind="item" />
   </section>
 </template>
 <style scoped lang="scss">
